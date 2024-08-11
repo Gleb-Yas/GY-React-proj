24c00a82e707c586a5bd4cd83584ac8981075a36
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-08-11 18:01:34 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,44 +24,38 @@ export default function App() {
 
   return (
     <div>
+      <h1>Best selling</h1>
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error" outlined>
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success" elevated>
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning" outlined elevated>
+        Please update your profile contact information
+      </Alert>
+      <CustomButton message="Playing music!">Play some music</CustomButton>
+      <CustomButton message="Uploading your data!">Upload data</CustomButton>
+
+      {/* useState / useEffect */}
+
       <button onClick={() => setClicks(clicks + 1)}>
         You clicked {clicks} times
       </button>
       <button onClick={() => setClicks(0)}>Reset</button>
     </div>
-
-    // <div>
-    //   <h1>Best selling</h1>
-    //   <Product
-    //     name="Tacos With Lime"
-    //     imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-    //     price={10.99}
-    //   />
-    //   <Product
-    //     name="Fries and Burger"
-    //     imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-    //     price={14.29}
-    //   />
-    //   <Alert variant="info">
-    //     Would you like to browse our recommended products?
-    //   </Alert>
-    //   <Alert variant="error" outlined>
-    //     There was an error during your last transaction
-    //   </Alert>
-    //   <Alert variant="success" elevated>
-    //     Payment received, thank you for your purchase
-    //   </Alert>
-    //   <Alert variant="warning" outlined elevated>
-    //     Please update your profile contact information
-    //   </Alert>
-    //   <CustomButton message="Playing music!">Play some music</CustomButton>
-    //   <CustomButton message="Uploading your data!">Upload data</CustomButton>
-
-    //   {/* useState / useEffect */}
-
-    //   <button onClick={() => setClicks(clicks + 1)}>
-    //     You clicked {clicks} times
-    //   </button>
-    // </div>
   );
 }
