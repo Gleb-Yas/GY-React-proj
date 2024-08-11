5a27a212b7293411c23262b64bc308d8e8cdb5c4
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-08-11 17:36:41 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,57 @@
 import { Alert } from './Alert/Alert';
+import CustomButton from './Button/Button';
+import Modal from './Modal/Modal';
 import Product from './Product/Product';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  // const [clicks, setClicks] = useState(0);
+
+  // useEffect(() => {
+  //   document.title = `You clicked ${clicks} times`;
+  // }, [clicks]);
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
-      <h1>Best selling</h1>
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-      <Alert variant="info">
-        Would you like to browse our recommended products?
-      </Alert>
-      <Alert variant="error" outlined>
-        There was an error during your last transaction
-      </Alert>
-      <Alert variant="success" elevated>
-        Payment received, thank you for your purchase
-      </Alert>
-      <Alert variant="warning" outlined elevated>
-        Please update your profile contact information
-      </Alert>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? 'Close' : 'Open'}
+      </button>
+      {isOpen && <Modal />}
     </div>
+
+    // <div>
+    //   <h1>Best selling</h1>
+    //   <Product
+    //     name="Tacos With Lime"
+    //     imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+    //     price={10.99}
+    //   />
+    //   <Product
+    //     name="Fries and Burger"
+    //     imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+    //     price={14.29}
+    //   />
+    //   <Alert variant="info">
+    //     Would you like to browse our recommended products?
+    //   </Alert>
+    //   <Alert variant="error" outlined>
+    //     There was an error during your last transaction
+    //   </Alert>
+    //   <Alert variant="success" elevated>
+    //     Payment received, thank you for your purchase
+    //   </Alert>
+    //   <Alert variant="warning" outlined elevated>
+    //     Please update your profile contact information
+    //   </Alert>
+    //   <CustomButton message="Playing music!">Play some music</CustomButton>
+    //   <CustomButton message="Uploading your data!">Upload data</CustomButton>
+
+    //   {/* useState / useEffect */}
+
+    //   <button onClick={() => setClicks(clicks + 1)}>
+    //     You clicked {clicks} times
+    //   </button>
+    // </div>
   );
 }
